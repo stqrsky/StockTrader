99f59deb8fdf90f18a1f5a8dcb3076dae8ca4537
star <star@starbook.fritz.box>
2020-08-05 17:25:21 +0200

    routes config #2 + 1st components

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="container">
-  
+        <router-view></router-view>
   </div>
 </template>
 
--- src/main.js
+++ src/main.js
@@ -2,10 +2,17 @@ import Vue from 'vue'
 import VueRouter from 'vue-router';
 
 import App from './App.vue'
+import { routes } from './routes';
 
 Vue.use(VueRouter);
 
+const router = new VueRouter({
+  mode: 'history',      // URLs , no #
+  routes                // routes: routes, ES6-syntax used
+});
+
 new Vue({
   el: '#app',
+  router,               // routes: routes, ES6-syntax used
   render: h => h(App)
 })
