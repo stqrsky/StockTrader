faaf508df51deb4f3b3f8002801ae951b8eaa0b1
star <star@starbook.fritz.box>
2020-08-05 18:07:31 +0200

    dummy data

--- src/components/stocks/Stocks.vue
+++ src/components/stocks/Stocks.vue
@@ -1,3 +1,18 @@
 <template>
     <h1>The Stocks Component</h1>
 </template>
+
+<script>
+    export default {
+        data() {
+            return {
+                stocks: [
+                    { id: 1, name: 'Nike', price: 110 },
+                    { id: 2, name: 'Inditex', price: 200 },
+                    { id: 3, name: 'LVMH', price: 250 },
+                    { id: 4, name: 'TJX', price: 8 },
+                ]
+            }
+        }
+    }
+</script>
