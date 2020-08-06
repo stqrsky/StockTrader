bc74dbe4285563985c787c74dc74cdf1b91ec934
star <star@starbook.fritz.box>
2020-08-06 10:07:58 +0200

    Vuex state management

--- src/store/modules/stocks.js
+++ src/store/modules/stocks.js
@@ -19,5 +19,21 @@ const actions = {
     },
     initStocks: ({ commit }) => {       // initalize stocks
         commit('SET_STOCKS', stocks)
+    }, 
+    randomizeStocks: ({commit}) => {
+        commit('RND_STOCKS');
     }
 };
+
+const getters = {
+    stocks: state => {
+        return state. stocks;
+    }
+};
+
+export default {
+    state,
+    mutations,
+    actions,
+    getters
+}
