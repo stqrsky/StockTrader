1805908ebe4c139f28c6aff3a1126264ad6ed9b5
star <star@starbook.fritz.box>
2020-08-06 17:24:11 +0200

    actions to commit , and getters (stockPort, funds)

--- src/store/modules/portfolio.js
+++ src/store/modules/portfolio.js
@@ -26,3 +26,33 @@ const mutations ={
         state.funds += stockPrice * quantity;
     }
 };
+
+const actions = {
+    sellStock({commit}, order) {
+        commit('SELL_STOCK', order);
+    }
+};
+
+const getters = {
+    stockPortfolio (state, getters) {
+        return state.stocks.map(stock => {                                                  // transform each element in array
+            const record = getters.stocks.find(element => element.id == stock.id);          // getter from the ..modules/stocks.js
+            return {
+                id: stock.id,
+                quantity: stock.quantity,
+                name: record.name,
+                price: record.price
+            }                                                                               // new array where i mapped all values with name & price additionally
+        });              
+    },
+    funds (state) {
+        return state.funds;
+    }
+};
+
+export default {
+    state,
+    mutations,
+    actions,
+    getters
+}
