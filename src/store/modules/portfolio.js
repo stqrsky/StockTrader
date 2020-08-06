0254b6f460942a4e3bf183095ad22277f9959076
star <star@starbook.fritz.box>
2020-08-06 17:10:10 +0200

    update funds for BUY_STOCK and SELL_STOCK

--- src/store/modules/portfolio.js
+++ src/store/modules/portfolio.js
@@ -14,6 +14,7 @@ const mutations ={
                 quantity: quantity
             });
         }
+        state.funds -= stockPrice * quantity;
     },
     'SELL_STOCK' (state, {stockId, quantity, stockPrice}) {
         const record = state.stocks.find(element => element.id == stockId);
@@ -22,5 +23,6 @@ const mutations ={
         } else {
             state.stocks.splice(state.stocks.indexOf(record), 1);
         }
+        state.funds += stockPrice * quantity;
     }
 };
