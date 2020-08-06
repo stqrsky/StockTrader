da99411b3fbafe60f5a66bef18ae823af0e8bacc
star <star@starbook.fritz.box>
2020-08-06 17:07:59 +0200

    portfolio - BUY-STOCK SELL_STOCK

---
+++ src/store/modules/portfolio.js
@@ -0,0 +1,26 @@
+const state = {
+    funds: 10000,
+    stocks: []
+};
+
+const mutations ={
+    'BUY_STOCK'(state, {stockId, quantity, stockPrive}) {         // the needs to commit a buy stock mutation
+        const record = state.stocks.find(element => element.id == stockId);
+        if (record) {
+            record.quantity += quantity;
+        } else {
+            state.stocks.push({
+                id: stockId,
+                quantity: quantity
+            });
+        }
+    },
+    'SELL_STOCK' (state, {stockId, quantity, stockPrice}) {
+        const record = state.stocks.find(element => element.id == stockId);
+        if (record.quantity > quantity) {
+            record.quantity -= quantity;
+        } else {
+            state.stocks.splice(state.stocks.indexOf(record), 1);
+        }
+    }
+};
