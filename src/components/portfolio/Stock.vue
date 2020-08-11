863e3ec7793f20708a58819b6834cac4d7775189
star <star@starbook.fritz.box>
2020-08-11 17:53:13 +0200

    styling sell button

--- src/components/portfolio/Stock.vue
+++ src/components/portfolio/Stock.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="col-sm-6 col-md-4">
-        <div class="panel panel-success">
+        <div class="panel panel-info">
             <div class="panel-heading">
                 <h3 class="panel-title">
                     {{ stock.name }}
