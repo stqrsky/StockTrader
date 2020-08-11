cc0006a4cc79e48363650edff0f25b05e7f58243
star <star@starbook.fritz.box>
2020-08-11 18:24:51 +0200

    error message , order checks

--- src/components/portfolio/Stock.vue
+++ src/components/portfolio/Stock.vue
@@ -14,20 +14,27 @@
                             class="form-control"
                             placeholder="Quantity"
                             v-model.number="quantity"
+                            :class="{danger: insufficientQuantity}"
                     >
                 </div>
                 <div class="pull-right">
                     <button 
                             class="btn btn-success"
                             @click="sellStock"
-                            :disabled="quantity <= 0 || !Number.isInteger(quantity)"
-                    >Sell
+                            :disabled="insufficientQuantity || quantity <= 0 || !Number.isInteger(quantity)"
+                    >{{ insufficientQuantity ? 'Not enough' : 'Sell' }}
                     </button>
                 </div>
             </div>
         </div>
     </div>
 </template>
+
+<style scoped>
+    .danger {
+        border: 1px solid red;
+    }
+</style>
 
 <script>
     import {mapActions} from 'vuex';
@@ -37,6 +44,11 @@
         data() {
             return {
                 quantity: 0
+            }
+        },
+        computed: {
+            insufficientQuantity() {
+                return this.quantity > this.stock.quantity;
             }
         },
         methods: {
